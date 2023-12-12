016f6e062fac4221aeb8cf474188ab9e133f84f5
Sankalp Tiwari <41205888+SankalpSTiwari@users.noreply.github.com>
2023-12-12 13:01:51 -0800

    Created Components

---
+++ src/components/Input.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Input = (props) => {
+  return (
+    <input
+      placeholder={props.placeholder}
+      value={props.value}
+      onChange={props.onChange}
+    />
+  );
+};
+
+export default Input;
