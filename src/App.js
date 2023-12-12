879b92cb4303cc66fb55d76f4bcf4187384c280d
Sankalp Tiwari <41205888+SankalpSTiwari@users.noreply.github.com>
2023-12-12 13:22:01 -0800

    Added Styling to Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
+import Button from './components/Button';
+import Input from './components/Input';
 
 function App() {
   return (
     <div className='App'>
       <h1>Weather Forecast</h1>
+      <Input />
+      <Button value='Search' />
+      <Card />
+      <Button value='Refresh' />
     </div>
   );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Input = (props) => {
   return (
     <input
+      className='input-field'
       placeholder={props.placeholder}
       value={props.value}
       onChange={props.onChange}
